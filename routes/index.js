3c7f603e2db96fb8aed88682779a0861ce25c9a9
Razmik Hakobyan <razhakobyan@gmail.com>
2018-04-28 22:19:16 -0700

    init db

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const db = require('../db');
+
+
+router.get("/", (request, response) => {
+    response.render('index', {
+        data:'Hello'
+    });
+});
+/*
+db.any(`INSERT INTO club ("cid", "age", "team", "city" ) VALUES (3522, 100, 'Manchester FC', 'Manchester, England')`)
+    .then( _ => db.any(`SELECT * FROM club`) )
+    .then(data => response.json('index', {title: 'Index', data: data}))
+    .catch( error => {
+        console.log( error );
+        response.json({ error });
+    })*/
+module.exports = router;
