1fb903c0bc793a2e5ad31f7f42d42a6890888ed2
razmikh <razhakobyan@gmail.com>
2018-05-02 00:49:50 -0700

    complete interface

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
 
+const bodyParser = require('body-parser')
 const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const favicon = require('serve-favicon');
 const expressHb = require('express-handlebars');
+const expressValidator = require('express-validator');
+const session = require('express-session');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');;
+const path = require('path');
+const favicon = require('serve-favicon');
+const flash = require('connect-flash');
 
 
 const index = require('./routes/index');
@@ -14,9 +17,20 @@ const index = require('./routes/index');
 const app = express();
 
 // view engine setup
+const hbsHelpers = expressHb.create({
+    helpers: {
+        getStringJSON: (content) =>{
+            return JSON.stringify(content);
+        }
+    },
+    defaultLayout:'layout',
+    extname: '.handlebars'
+});
+
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', expressHb({defaultLayout: 'layout'}));
+app.engine('handlebars', hbsHelpers.engine);
 app.set('view engine', 'handlebars');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -26,6 +40,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+app.use(expressValidator({
+    errorFormatter: (param, msg, value) => {
+        const namespace = param.split('.')
+            , root = namespace.shift()
+            , formParam = root;
+
+        while(namespace.length){
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+}));
+
+app.use(flash());
+app.use((req, res, next) =>{
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
 
 app.use('/', index);
 
--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,34 @@ const express = require("express");
 const router = express.Router();
 const db = require('../db');
 
-
 router.get("/", (request, response) => {
-    response.render('index', {
-        data:'Hello!'
-    });
+    response.render('index');
 });
-/*
-db.any(`INSERT INTO club ("cid", "age", "team", "city" ) VALUES (3522, 100, 'Manchester FC', 'Manchester, England')`)
-    .then( _ => db.any(`SELECT * FROM club`) )
-    .then(data => response.json('index', {title: 'Index', data: data}))
-    .catch( error => {
-        console.log( error );
-        response.json({ error });
-    })*/
+
+router.get("/:query", (request, response) => {
+    response.render('index',{query:request.params.query});
+});
+router.post("/:query", (request, response) => {
+    const query = request.body.query;
+
+    request.checkBody('query', 'Please input a search query').notEmpty();
+    const errors = request.validationErrors();
+
+    if (errors)
+        response.render('', {
+            errors: errors
+        });
+    else{
+        db.any(query).then(data => {
+            console.log('Data' + JSON.stringify(data));
+            request.flash('success_msg', 'Successful query');
+            response.redirect('/'+ JSON.stringify(data));
+
+        }).catch(errors =>{
+            request.flash('error_msg', '' + errors);
+            response.redirect('/');
+        })
+    }
+});
+
 module.exports = router;
